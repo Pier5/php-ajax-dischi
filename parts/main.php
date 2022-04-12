6f4e50ea82a15b80c2801c10017a4e61a1e9670b
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-04-12 15:57:54 +0200

    -aggiunto stile-

--- parts/main.php
+++ parts/main.php
@@ -3,7 +3,7 @@
         <?php
         foreach ($arrAlbum as $info) { ?>
             <div class="card">
-            <img src="<?= $info['poster'] ?>" alt="<?= $info['title'] ?>">
+            <img class="main-card" src="<?= $info['poster'] ?>" alt="<?= $info['title'] ?>">
             <h2><?= $info['title'] ?></h2>
             <h3><?= $info['author'] ?></h3>
             <h4><?= $info['year'] ?></h4>
