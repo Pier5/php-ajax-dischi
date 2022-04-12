520fc0c97ae2cf57ffcc607bdf3f722317eff63e
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-04-12 17:12:13 +0200

    -opzioni generate in maniera dinamica- /funzione per filtrare i doppioni

--- frontend/script.js
+++ frontend/script.js
@@ -2,10 +2,20 @@ new Vue({
     el: '#app',
     data: {
         arrAlbum : [],
+        arrOptions : []
     },
     created() {
         axios.get('http://localhost/php-ajax-dischi/api.php')
         .then(response => this.arrAlbum = response.data)
+    },
+    computed: {
+        filterArrOptions () {
+          this.arrAlbum.forEach((el) => {
+            if (!this.arrOptions.includes(el.genre)) {
+              this.arrOptions.push(el.genre)
+            }
+          })
+          return this.arrOptions
+        }
     }
-
 })
